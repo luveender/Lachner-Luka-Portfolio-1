5592d4ae220b2f4a0461a372c9305d306da21695
Luka Lachner <mojoloom@gmail.com>
2025-04-17 20:48:51 -0600

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@
 
 const data = {
     name: "Luka Lachner",
-    bio: "Bachelor of Design and City Innovation Student at University of Calgary looking to integrate ecological systems with the built environment.",
+    bio: "Bachelor of Design and City Innovation student at University of Calgary looking to integrate ecological systems with the built environment.",
     contact: {
         email: "luka.lachner@ucalgary.ca",
         phone: "620-468-8646",
